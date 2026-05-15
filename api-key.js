32f90bf06a8aa9737ff4241225cfb10e7c2f7c9a
deck1984-alt <deck1984@gmail.com>
2026-05-15 11:48:50 +0800

    Update api-key.js

--- api-key.js
+++ api-key.js
@@ -1,6 +1,7 @@
 // 私人 API Key 設定
 const API_CONFIG = {
     GEMINI_KEY: "AIzaSyB7Ju-UnZ9TrmHuCY5ri0eiup216gp0aYg",
+    DEBUG_MODE_TRIGGER:"ADHDFocusLAB",
     // 如果你有不同的專案環境，也可以分開寫
     VERSION: "v5.3-Stable"
 };
